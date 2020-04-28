c8b04aad524dd9cd4937283d3d9c257010629c38
Jeannine Brokaw <49129316+JIBrokaw@users.noreply.github.com>
2020-04-28 01:15:13 -0400

    Update Sample_Family_Record.py

--- Sample_Family_Record.py
+++ Sample_Family_Record.py
@@ -1,97 +1,15 @@
-#Post-formatting format needed [Name, Nicknames, Year, Big, Families, [Voice Part, Officer]]
-#Storage format ['Name', ['Nickname'], 'Year', ['Big'], ['Family name'], 'Voice Part', ['Officer position (years)']]
+#['Name', ['Nicknames'], 'Year', ['Big'], ['Families'], ['Fandom']]
 People = {
-    2014: [
-        ['Chantalle Ashford', ['Chantalle'], '2014', [], ['u'], 'S2', ['Vice President (2012-2013)', 'President (2013-2014)']]
-    ],
-    2015: [
-        #Sopranos
-        ['Heather Jones', ['Heather'], '2015', [], ['u'], 'S1', ['President (2014-2015)']]
-        #Tenors
-        ['John Sullivan', ['John', 'Sully'], '2015', [], ['Discomforture'], 'T1', []]
-    ],
-    #2016
-    2016: [
-        #Sopranos
-        ['Sarah Falls', ['Sarah'], '(entered with class of 2016)', [], ['u'], 'S1', ['Wardrobe Chair (2016-2017)']], ['Taylor Horst', ['Taylor'], '2016', [], ['DOJ', 'Name-Change'], 'S1', ['Vice President (2014-2015)','President (2015-2016)']], ['Christine Jacobs', ['Christine'], '2016', [], ['u'], 'S1', []], ['Emma Pierce', ['Emma'], '2016', [], ['u'], 'S1', []], ['Meaghan Smith', ['Meaghan'], '2016', [], ['u'], 'S1', []], ['Emily McLenigan', ['Emily'], '2016', [], ['u'], 'S2', []], ['Alyssa Melchers', ['Alyssa'], '2016', [], ['Name-Change', 'Shaker-Down'], 'S2', []], ['Austin Spivey', ['Austin'], '2016', [], ['u'], 'S2', []], ['Abby Thompson', ['Abigail', 'Abby'], '2016', [], ['u'], 'S2', []],
-        #Altos
-        ['Lauren Fernandez', ['Lauren'], '2016', [], ['u'], 'A1', []], ['Claire Tinsley', ['Claire'], '2016', [], ['u'], 'A1', []], ['Allison Doane', ['Allison'], '2016', [], ['u'], 'A1', []], ['Rachel Haynes', ['Rachel'], '2016', [], ['u'], 'A2', []],
-        #Tenors
-        ['Stephen Cotner', ['Stephen'], '2016', [], ['u'], 'T1', ['Wardrobe Chair (2015-2016)']], ['Dereck Basinger', ['Dereck'], '2016', ['Chantalle Ashford'], [], 'T2', ['Recording Secretary (2015-2016)']],
-        #Basses
-        ['Connor Barto', ['Connor'], '2016', [], ['u'], 'B1', []], ['Shupeng Cao', ['Shupeng'], '2016', [], ['u'], 'B1', []], ['Alex Foster', ['Alex'], '2016', [], ['u'], 'B1', []], ['Yuntian Guan', ['Yuntian'], '2016', [], ['u'], 'B1', []], ['Daniel Perkes', ['Daniel'], '2016', [], ['DOJ'], 'B1', []], ['Jacob Robins', ['Jacob'], '2016', [], ['u'], 'B1', []], ['Alexander Schwartz', ['Alexander'], '2016', [], ['u'], 'B2/T2', []], ['Abhinav Venkat', ['Abhinav', 'Abi'], '2017 (entered with class of 2016)', [], ['Name-Change','Shaker-Down Sun'], 'B2/T2', ['Treasurer (2015-2016)']],
-        ],
-    2017: [
-        #Sopranos
-        ['Elaine Edwards', ['Elaine'], '2017', [], ['u'], 'S1', []], ['Allison Giles', ['Allison'], '2017', [], ['u'], 'S1', []], ['Catherine Grundy', ['Catherine'], '2017', [], ['u'], 'S1', []], ['McKenna Hunt', ['McKenna'], '2017', ['Jessie Partridge'], [], 'S1', []], ['Marianna Stepniak', ['Marianna'], '2017', [], ['u'], 'S1', []], ['Sana Amado', ['Sana'], '2017', [], ['u'], 'S2', []], ['Sophia Farion', ['Sophia'], '2017', [], ['u'], 'S2', []], ['Jessie Partridge', ['Jessie'], '2017', [], ['DOJ'], 'S2', []], ['Nairuti Shastry', ['Nairuti'], '2017', [], ['u'], 'S2', []],
-        #Altos
-        ['Rachel Azafrani', ['Rachel'], '2017', [], ['u'], 'A1', []], ['Emily Chrisman', ['Emily'], '2017', [], ['u'], 'A1', []], ['Victoria Johnson', ['Victoria'], '2017', [], ['u'], 'A1', []], ['Hope Trisler', ['Hope'], '2017', [], ['u'], 'A1', ['Social Chair (2015-2016)']], ['Colleen Fennessy', ['Colleen'], '2017', [], ['u'], 'A2', []], ['Lindsey Hughes', ['Lindsey'], '2017', [], ['u'], 'A2', []], ['Emily Wasek', ['Emily'], '2017', ['Alyssa Melchers'], [], 'A2', ['Wardrobe Chair (2014-2015)', 'Alumni Correspondent (2015-2016)']],
-        #Tenors
-        ['William Gaskins', ['William'], '2017', [], ['u'], 'T1', []], ['John da Silva Passos Hoioos', ['John', 'Jake', 'John da Silva Passos Hoioos'], '2017', [], ['u'], 'T1', ['Recording Chair (2015-2016)']], ['Nathaniel Clemens', ['Nathaniel'], '2017', [], ['u'], 'T2', []], ['Pat Firth', ['Patrick', 'Pat'], '2017', [], ['u'], 'T2', ['Social Chair (2015-2016)']], ['Samuel Holliday', ['Sam', 'Samuel'], '2017', ['John Sullivan'], [], 'T2', ['Vice President (2015-2016)', 'President (2016-2017)']], ["James O'Duden", ['James'], '2017', [], ['u'], 'T2', []],
-        #Basses
-        ['Alexander DiLauro', ['Alexander'], '2017', [], ['u'], 'B1', []], ['Thomas Kalnas', ['Thomas'], '2017', [], ['u'], 'B1', []], ['Jay Sandridge', ['Jay', 'Hugh'], '2017', [], ['u'], 'B1', ['Webmaster (2015-2016)']], ['William Theuer', ['William'], '2017', [], ['u'], 'B1', []], ['Sebastian Tsai', ['Sebastian'], '2017', ['John Sullivan'], [], 'B1', ['Librarian (2015-2017)']], ['Joshua Frakes', ['Joshua'], '2017', [], ['u'], 'B2', []], ['Jacob Herrin', ['Jacob'], '2017', [], ['u'], 'B2', []], ['Austin Morgan', ['Austin'], '2017', [], ['u'], 'B2', []]
-        ],
-    2018: [
-        #Sopranos
-        ['Roxy Moran', ['Roxy'], '2018', ['Jessica Campbell'], [], 'S1', []], ['Jessica Campbell', ['Jessica'], '2018', ['Abhinav Venkat'], [], 'S2', ['Historian (2015-2016)']], ['Lizzy Lewis', ['Elizabeth', 'Lizzy'],  '2018', [], ['u'], 'S2', []],
-        #Altos
-        ['Meg Collins', ['Margaret', 'Meg'], '2018', [], ['DOJ'], 'A1', ['Historian (2015-2016)', 'Vice President (2016-2017)', 'President (2017-2018)']], ['Mikaela Richardson', ['Mikaela'], '2018', [], ['u'], 'A1', []], ['Sarah Pittman', ['Sarah'], '2018', [], ['u'], 'A1', []], ['Adrienne Thompson', ['Adrienne'], '2018', [], ['u'], 'A1', []], ['Sam Wooley', ['Samantha', 'Sam'], '2018', [], ['u'], 'A1', ['Wardrobe Chair (2015-2016)']], ['Grace Keener', ['Grace'], '2018', [], ['u'], 'A2', []], ['Hannah Kwawu', ['Hannah'], '2018', [], ['u'], 'A2', []], ['Bailey Orr', ['Bailey'], '2018', ['Taylor Horst'], ['u'], 'A2', []],
-        #Tenors
-        ['Andy Li', ["Andy"], '2018', [], ['u'], 'T1', ['Fundraising Chair (2015-2016)']], ['Matthew Spears-Heinel', ['Matthew', 'Matt'], '2018', [], ['u'], 'T2/T1', ['Fundraising Chair (2015-2016)']], ['Ethan Buck', ['Ethan'], '2018', [], ['u'], 'T2', []], ['Luc Nguyen', ['Luc'], '2018', [], ['u'], 'T2', []], ['Dolan Harrington', ['Dolan'], '2018', ['William Gaskins'], [], 'T2', []],
-        #Basses
-        ['Brian Anyakoha', ['Brian'], '2018', [], ['u'], 'B2', []], ['Lea Booth', ['George', 'Lea'], '2019 (entered with class of 2018)',  [], ['u'], 'B2', []], ['Connor Knutsen', ['Connor'], '2018', ['Jessica Campbell'], [], 'B2', []], ['Jeffrey Wang', ['Jeffrey', 'Qingsen'], '2018 (Grad student)', [], ['u'], 'B2', []]
-        ],
     2019: [
-        #Sopranos
-        ['Caroline Abbott', ['Caroline'], '2019', ['Abhinav Venkat', 'Jessica Campbell'], [], 'Unknown', []], ['Allie Diemer', ['Allie'], '2019', [], ['u'], 'S1', []], ['Amy Folkerts', ['Amy'], '2019', [], ['u'], 'S1', []], ['Kayla Ebright', ['Kayla'], '2019', [], ['u'], 'S1', ['Treasurer (2018-2019)']], ['Mina Parastaran', ['Mina'], '2019', [], ['DOJ', "Name-Change"], 'S1', ['Historian (2016-2017)', 'Hawtie (2018-2019)']], ['Virginia Tilley', ['Virginia'], '2019', ['Hannah Durfee', 'Bailey Orr'], [], 'S2/S1', ['Librarian (2017-2018)']], ['Rebecca Hall', ['Rebecca'], '2019', [], ['Unknown'], 'S2', []], ['Tess Kendrick', ['Tess'], '2019',  [], ['DOJ'], 'S2', ['Riser Queen (2015-2016)', 'Historian (2016-2017)', 'Vice President (2017-2018)', 'President (2018-2019)']], ['Morgan Massa', ['Morgan'], '2019', [], ['u'], 'S2', []],
-        #Altos
-        ['Hannah Durfee', ['Hannah', 'Durf'], '2020 (entered with class of 2019)', ['McKenna Hunt'], [], 'S2/A1', ['Social Chair (2019-2020)']], ['Lucy Chen', ['Lucy'], '2019', ['Mina Parastaran'], [], 'A1', []], ['Julia Wicks', ['Julia'],'2019', ['Jessie Partridge'], [], 'A1', []], ['Nora Logsdon', ['Nora'], '2019',  [], ['u'], 'A2', ['Alumni Correspondent (2018-2019)']], ['Shana Merker', ['Shana'], '2019', [], ['u'], 'A2/T1', []], ['Sarah Salem', ['Sarah'], '2019', [], ['u'], 'Unknown', []],
-        #Tenors
-        ['Garret Walker', ['Garret'], '2019', [], ['DOJ', 'Discomforture', 'Name-Change'], 'T1', ['Social Chair (2018-2019)']], ['Theocles Herrin', ['Theocles'], '2019', [], ['u'], 'T2', []],
-        #Basses
-        ['Kyle Cochran', ['Kyle'], '2019', [], ['u'], 'B1', []], ['Michael Sparrow', ['Michael'], '2019', ['Daniel Perkes'], [], 'B1', ['Webmaster (2016-2019)']], ['Sebastian Viscuso', ['Sebastian'], '2019', ['Abhinav Venkat', 'Jessica Campbell'], [], 'B1', ['Wardrobe Chair (2016-2019)']], ['Robert Wiley', ['Robert', 'Bob'], '2019', [], ['u'], 'B1/B2', []], ['David McCreight', ['David', 'Wils'], '2019', [], ['u'], 'B2', []]
-        ],
+        ['The Doctor', ['Who', 'Doctor Who'], '2019', [], ['Time Lords'], ['Doctor Who']], ['Shmi Skywalker', [], '2019', [], ['Skywalker Clan'], ['Star Wars']], ['Emperor Palpatine', ['Sheev','Chancellor'], '2019', [], ['Palpatine Line'], ['Star Wars']], ['Sebastian Viscuso', [], '2019', [], ['Name Change', 'Shaker-Down Sun'], ['Kwah']]
+    ],
     2020: [
-        #Sopranos
-        ['Bianca Bowman', ['Bianca'], '2020', [], ['U'], 'S1', []], ['Sarah Lucas', ['Sarah'], '2020', [], ['DOJ', 'Name-Change', 'Lunch Bunch'], 'S2', ['Fundraising Chair (2018-2019)', 'Hawtie (2019-2020)']], ['Sara Richard', ['Sara'], '2020', ['Emily Wasek'], ['Name-Change', 'Shaker-Down Sun'], 'S2',  ['Social Chair (2018-2019)', 'I-Tour Manager (2019-2020)']],
-        #Altos
-        ['Isandro Gallego-Garcia', ['Isandro', 'Izzy'], '2020', [], ['U'], 'A1', []], ['Annika Hackfield', ['Annika'], '2020', ['Emma Jones'], [], 'A2', []],
-        ['Sheng Long', ['Sheng'], '2020', ['Sebastian Viscuso'], [], 'A2', ['Librarian (2019-2020)']],
-        #Tenors
-        ['Joseph Allan', ['Joseph'], '2020', ['Sebastian Tsai'], [], 'T1', ['Social Chair (2019-2020)']], ['Xavier Soto Burgos', ['Xavier'], '2020', [], ["u"], 'T1', []], ['Spencer Leibow', ['Spencer'], '2020', [], ['U'], 'T1', []], ['James Lynch', ['James'], '2020', [], ['Discomforture', 'Lunch Bunch'], 'T2', ['Fundraising Chair (2018-2019)']],
-        #Basses
-        ['Nicholas Wells', ['Nicholas', 'Nick'], '2020', ['Samuel Holliday'], [], 'B1/T2', ['Sports Chair (2017-2018)', 'Vice President (2018-2019)', 'President (2019-2020)']], ['Palmer Foran', ['Palmer', 'The Mono guy'], '2020', [], ['U'], 'B1', ['Social Chair (2017-2018)']], ['Harrison Tuckman', ['Harrison'], '2020', [], ['U'], 'B2', ['Treasurer (2019-2020)']]
-        ],
+        ['Padme Amidala', [], '2020', [], [''], ['Star Wars']], ['Lily Evans', [], '2020', [], [''], ['Harry Potter']], ['Sheng Long', [], '2020', ['Sebastian Viscuso'], [], ['Kwah']], ['James Potter', ['Prongs'], '2020', [], ['The Potters'], ['Harry Potter']], ['Molly Prewett', [], '2020', [], [''], ['Harry Potter']], ['Anakin Skywalker', ['Darth Vader'], '2020', ['Shmi Skywalker'], [], ['Star Wars']], ['Arthur Weasley', [], '2020', [], ['Weasley Clan'], ['Harry Potter']], ['River Song', ['Melody Pond'], '2020', ['Amy Pond', 'Rory Williams'], [], ['Doctor Who']]
+    ],
     2021: [
-        #Sopranos
-        ['Aria Austin', ['Aria'], '2021', [], ['U'], 'S1', []], ['Olivia Levin', ['Olivia'], '2021', ['Lucy Chen'], [], 'S1', []], ['Amelia Lowe', ['Amelia'], '2021', ['James Lynch', 'Sarah Lucas'], [], 'S1', []], ['Mary Shannon', ['Mary'], '2021', ['Bailey Orr', 'Virginia Tilley'], ['Shaker-Down'], 'S1', ['Wardrobe Chair (2019-2020)']], ['Emma Jones', ['Emma'], '2020 (entered with class of 2021)', ['Jessica Campbell'], [], 'S2', ['Historian (2018-2019)']], ['Cristina Sherer', ['Cristina'], '2021', [], ['Name-Change','Discomforture','DOJ','Shaker-Down Sun'], 'S2', ['Historian (2018-2019)']], ['Caroline Sublett', ['Caroline'], '2021', ['Meg Collins'], [], 'S2', ['Riser Queen (2017-2018)']], ['Rebecca Willis', ['Rebecca', 'Reese'], '2021', ['Sheng Long'], [], 'S2', []],
-        #Altos
-        ['Cassandra Fernandez', ['Cassandra'], '2021', [], ['U'], 'A1', []], ['Emma Shahin', ['Emma'], '2021', [], ['U'], 'A1',  ['Wardrobe Chair (2018-2019)']], ['Sahithya Vishwanath', ['Sahithya', 'Saha'], '2021', ['Sheng Long'], [], 'A1', []], ['Emma Ward', ['Emma'], '2021', [], ['U'], 'A1', []], ['Margot Cunningham', ['Margot'], '2021', [], ['U'], 'A2', ['Sports Chair (2018-2019)']], ['Meghan Ward', ['Meghan'], '2021', ['James Lynch', 'Sarah Lucas'], [], 'A2', []],
-        #Tenors
-        ['Kyle Vasquez', ['Kyle'], '2021', [], ['U'], 'T1', ['Recording Chair (2018-2019)', 'Wardrobe Chair (2019-2020)']], ['Zachery Amos', ['Zach', 'Zachery'], '2021', ['Sara Richard', 'Mary Shannon'], [], 'T1', []], ['Caleb Sanford', ['Caleb'], '2021', ['Nicholas Wells'], [], 'T2', ['Riser King (2018-2019)', 'Vice President (2019-2020)']],
-        #Basses
-        ['John Lesko', ['John'], '2021', [], ['U'], 'B1', ['Riser King (2017-2018)', 'Recording Chair (2018-2019)']], ['Ontario Turner', ['Ontario'], '2021', ['Owen Peck'], [], 'B1', []], ['Jacob Daum', ['Jacob'], '2021', ['Michael Sparrow'], [], 'B2', []]
-        ],
+        ['Young Palpatine', [], '2021', ['Emperor Palpatine'], [], ['Star Wars']], ['Amy Pond', ['The girl who waited'], '2021', [], ['Ponds'], ['Doctor Who']], ['Harry Potter', [], '2021', ['James Potter', 'Lily Evans'], [], ['Harry Potter']], ['Luke Skywalker', [], '2021', ['Anakin Skywalker', 'Padme Amidala'], [], ['Star Wars']], ['Leia Skywalker', [], '2021', ['Anakin Skywalker', 'Padme Amidala'], [], ['Star Wars']], ['Han Solo', [], '2021', [], ['Solos'], ['Star Wars']], ['Ginny Weasley', ['Ginevra'], '2021', ['Arthur Weasley', 'Molly Prewett'], [], ['Harry Potter']], ['Ron Weasley', ['Ronald'], '2021', ['Arthur Weasley', 'Molly Prewett'], [], ['Harry Potter']], ['Rory Williams', [], '2021', [], [''], ['Doctor Who']]
+    ],
     2022: [
-        #Sopranos
-        ['Lauren Grob', ['Lauren'], '2022', [], ['Unknwon'], 'S1', ['Alumni Correspondent (2019-2020)']], ['Abigail Greene', ['Abigail', 'Abby'], '2022', ['JaneAnne Stockton'], [], 'S1', []], ['JaneAnne Stockton', ['Jane Anne', 'Jane-Anne', 'JaneAnne'], '2022', ['Meghan Ward'], [], 'S1', []], ['Theresa Thomas', ['Theresa'], '2022', ['Mina Parastaran'], [], 'S1', ['Recording Chair (2019-2020)']], ['Laura Trowbridge', ['Laura'], '2022', ['Mary Shannon', 'Sara Richard'], [], 'S1', []], ['Margot Vanyan', ['Margot', 'Margaret'], '2022', [], ['Shaker-Down', 'Discomforture', 'Name-Change'], 'S1', ['Fundraising Chair (2019-2020)']], ['Margaret Delucia', ['Margaret'], '2022', [], ['Unknown'], 'S2', []], ['Caroline Emeric', ['Caroline'], '2022', ['Emma Shahin'], [], 'S2', []], ['Jordan Green', ['Jordan'], '2022', [], ['Unknown'], 'S2', ['Sports Chair (2019-2020)']], ['Belle Perkins', ['Belle'], '2022', ['Rebecca Hall'], [], 'S2', []], ['Alli Seifert', ['Allison', 'Alli'], '2022', ['Garret Walker'], [], 'S2', []], ['Hannah Simpson', ['Hannah'], '2022', ['Olivia Levin'], [], 'S2', ['Historian (2019-2020)']],
-        #Altos
-        ['Ceci Hughes', ['Ceci'], '2022', ['Emma Jones'], [], 'S2/A1', []], ['Jeannine Brokaw', ['Jeannine'], '2022', ['Sebastian Viscuso', 'Sheng Long'], [], 'A1', ['Librarian (2019-2020)']], ['Amelia Grossman', ['Amelia'], '2022', [], ['Unknown'], 'A1', []], ['Bisola Olowe', ['Bisola'], '2022', [], ['Unknown'], 'A1', []], ['Kirsten Sweigart', ['Kirsten'], '2022', ['Virginia Tilley'], [], 'A1', []], ['Mia Peri', ['Mia'], '2022', [], ['Unknown'], 'A2', []], ['Olga Zhugayevich', ['Olga'], '2022', ['Hannah Durfee'], [], 'A2', []],
-        #Tenors
-        ['Pablo Solano', ['Pablo'], '2022', ['James Lynch', 'Sarah Lucas'], [], 'T1', ['Webmaster (2019-2020)']], ['Joel McNett', ['Joel'], '2022', ['Nicholas Wells'], [], 'T2', []], ['Charlie Westhoff', ['Charles', 'Charlie'], '2022', ['Cristina Sherer'], [], 'T2', ['Fundraising Chair (2019-2020)']],
-        #Basses
-        ['Jay Ford', ['Jay'], '2022', ['James Lynch', 'Sarah Lucas'], [], 'B1', ['Historian (2019-2020)']], ['Thomas Hess', ['Thomas', 'Tommy'], '2022', ['Jacob Daum'], [], 'B1', []], ['Owen Peck', ['Owen'], '2022', ['Michael Sparrow'], [], 'B1', []], ['Tomas Sommer', ['Tomas'], '2022', ['Kyle Vasquez'], [], 'B1', []], ['Matthew Wright', ['Matthew', 'Matt'], '2022', ['Thomas Hess'], [], 'B1', []], ['Will Casale', ['Will'], '2022', [], ['Unknown'], 'B2', []],
-        ],
-    2023: [
-        #Sopranos
-        ['Morgan Sanders', ['Morgan'], '2023', ['Hannah Simpson'], [], 'S1', []], ['Hannah Bloom', ['Hannah'], '2023', ['Kirsten Sweigart'], [], 'S2', ['Riser Queen']], ['Millicent Brigaud', ['Millicent', 'Millie'], '2023', ['Lauren Grob'], [], 'S2', []], ['Morgan King', ['Morgan', 'Mary'], '2023', ['Sara Richard', 'Mary Shannon'], [], 'S2', []], ['Bethany Shears', ['Bethany'], '2023', ['Jay Ford'], [], 'S2', []], ['Mika Taylor', ['Mika'], '2023', ['Hannah Durfee'], [], 'S2', []], ['Sophia Williams', ['Sophia'], '2023', ['Alli Seifert', 'Charlie Westhoff'], [], 'S2', []],
-        #Altos
-        ['Emma Dubin', ['Emma'], '2023', ['Caleb Sanford'], [], 'A1', []], ['Natalie Spage', ['Natalie'], '2023', ['Jeannine Brokaw'], [], 'A1', []], ['Emily Elston', ['Emily'], '2023', ['Margot Cunningham', 'Joseph Allan'], [], 'A2', []], ['Lydia Hecmanczuk', ['Lydia'], '2023', ['Theresa Thomas', 'Jordan Green'],  [], 'A2', []], ['Catherine Janicki', ['Catherine'], '2023', ['Theresa Thomas', 'Jordan Green'],  [], 'A2', []],
-        #Tenors
-        ['Matthew Siroty', ['Matthew', 'Matt'], '2023', ['Pablo Solano', "Margot Vanyan"],  [], 'T1', []], ['Tristan Hurst', ['Michael', 'Tristan'], '2023', ['Margot Cunningham', 'Joseph Allan'],  [], 'T2', []],
-        #Basses
-        ['Cory Brower', ['Cory', 'Commodore Riley Brower'],  '2023', ['Jacob Daum'], [], 'B1', []], ['Christian Huggard', ['Christian'], '2023', ['Alli Seifert', "Charlie Westhoff"],  [], 'B1', []], ['Samuel Freeman', ['Samuel', 'Sam'], '2023', ['Thomas Hess'], [], 'B1', []], ['Aubrey Lay', ['Aubrey'], '2023', ['Pablo Solano', 'Margot Vanyan'], [], 'B1', ['Riser King (2019-2020)']], ['Emilio Luz-Ricca', ['Emilio'], '2023',['James Lynch', "Sarah Lucas"], [], 'B1', []],
+        ['Jeannine Brokaw', [], '2022', ['Sheng Long', 'Sebastian Viscuso'], [], ['Kwah']], ['Rey Palpatine', [], '2022', ['Young Palpatine'], [], ['Star Wars']], ['Albus Potter', ['Albus Severus'], '2022', ['Harry Potter', 'Ginny Weasley'], [], ['Harry Potter']], ['Ben Solo', [], '2022', ['Han Solo', 'Leia Skywalker'], [], ['Star Wars']]
     ]
-#alyssa melchers - emily wasek - sara richard
-#
 }
